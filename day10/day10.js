9dd05739856cf551a0bf8c52480603ce5b73e3b5
GoodPineApple <taemin3000@naver.com>
2026-03-12 11:30:09 +0900

    day10 init

---
+++ day10/day10.js
@@ -0,0 +1,10 @@
+let sum = 10 + 20;
+console.log(sum);   // 30
+let sub = 20 - 10;
+console.log(sub)    // 10
+let multi = 10 * 20;
+console.log(multi)  // 200
+let remain = 10 % 3;
+console.log(remain) // 1
+let expon = 2 ** 3; 
+console.log(expon)  // 8
