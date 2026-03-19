0b8183dae121ef240efc93eddefa7b7c878d2f79
GoodPineApple <taemin3000@naver.com>
2026-03-19 10:47:13 +0900

    day14 timer

--- day14/async-assignment/async_assignment.js
+++ day14/async-assignment/async_assignment.js
@@ -1,51 +1,75 @@
 const timerInput = document.querySelector("#timerInput");
 const startTimer = document.querySelector("#startTimer");
 const timerDisplay = document.querySelector("#timerDisplay");
+const MAX_SEC = 10;
 
 const resetTimerInput = () => {
   // timerInput 초기화
 };
 
 const resetTimerDisp = () => {
-  // timerDisplay 초기화
+  timerDisplay.innerHTML = "";
 };
 
 const showTimerSec = (sec) => {
   resetTimerDisp();
-  // timerDisplay에 sec 표시
+  const p = document.createElement("p");
+  p.textContent = `${sec}초`;
+  timerDisplay.appendChild(p);
 };
 
 const showTimerComplete = () => {
   resetTimerDisp();
-  // timerDisplay에 타이머 종료 메세지 표시
+  const p = document.createElement("p");
+  p.textContent = "타이머 종료!!!";
+  timerDisplay.appendChild(p);
 };
 
 const showTimerError = (message) => {
   // timerDisplay에 오류 메세지 표시
+  resetTimerDisp();
+  const p = document.createElement("p");
+  p.textContent = message;
+  p.classList.add("error");
+  timerDisplay.appendChild(p);
 };
 
 const processTimer = (sec) => {
+  console.log("processTimer", sec);
   showTimerSec(sec);
-  // 1초마다 반복되는 함수
+  startTimer.disabled = true;
+
   const timer = setInterval(() => {
-    // 1초마다 sec 감소,
     sec -= 1;
-    // sec이 0보다 크면 sec 표시
-    if (sec < 0) {
+    if (sec > 0) {
+      showTimerSec(sec);
+    }
+    if (sec <= 0) {
       clearInterval(timer);
-      // 타이머 종료 메세지 표시
+      showTimerComplete();
+      startTimer.disabled = false;
     }
   }, 1000);
 };
 
 // 타이머 시작
 function handleClickTimer() {
-  try {
-    // timer input에서 sec 가져오기
-    processTimer(time);
-  } catch (error) {
-    // 오류 메세지 출력
-    showTimerError(error.message);
-    resetTimerInput();
+  console.log("타이머 시작");
+
+  const sec = timerInput.value;
+  const secNum = Number(sec);
+  // 유효성 검증
+  if (isNaN(secNum)) {
+    showTimerError("숫자를 입력해주세요.");
+    return;
   }
+  if (secNum < 1 || secNum > MAX_SEC) {
+    showTimerError(`1에서 ${MAX_SEC} 사이의 숫자를 입력해주세요.`);
+    return;
+  }
+  processTimer(secNum);
+  resetTimerInput();
 }
+
+// startTimer.addEventListener("click", handleClickTimer);
+// startTimer.onclick = handleClickTimer;
