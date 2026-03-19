ca9eaaadf2401ef0ee2e2ceb3e91d7ab3eebb5af
GoodPineApple <taemin3000@naver.com>
2026-03-19 13:06:10 +0900

    day15 exception input

--- day15/exception/input.js
+++ day15/exception/input.js
@@ -1,4 +1,12 @@
-const input = 1;
+const input = "fdfdfdf";
 
-const number = Number(input);
-console.log(number);
+try {
+  const number = Number(input);
+  console.log(number);
+  if (isNaN(number)) throw new Error("Input is not a number");
+  const num2 = number / 4;
+  console.log(num2);
+} catch (error) {
+  console.error("An error occurred:", error);
+  alert("Please enter a valid number");
+}
