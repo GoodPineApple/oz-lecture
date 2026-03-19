0eee92b46937b0b13aee4e8a11ec44972b35f27b
GoodPineApple <taemin3000@naver.com>
2026-03-19 10:56:27 +0900

    day14 fetch

--- day14/async-assignment-challenge/async_assignment_challenge.js
+++ day14/async-assignment-challenge/async_assignment_challenge.js
@@ -25,13 +25,29 @@ const fetchMultiplePosts = async (postIds) => {
   for (const postId of postIds) {
     const post = await fetchPost(postId);
     posts.push(post);
+    console.log(posts);
+  }
+  while (posts.length > 0) {
+    const post = posts.shift();
+    if (post) {
+      const postElement = document.createElement("div");
+      postElement.classList.add("card", "mb-3");
+      postElement.innerHTML = `
+        <div class="card-body">
+          <h5 class="card-title">${post.title}</h5>
+          <p class="card-text">${post.body}</p>
+        </div>
+      `;
+      output.appendChild(postElement);
+    }
   }
   // posts 배열을 순회하며 post 정보 출력
 };
 
 const handleClickSearch = async () => {
-  // inputPostIds에서 postIds 가져오기
-  await fetchMultiplePosts(postIds);
+  const postIds = inputPostIds.value;
+  const postIdsArr = postIds.split(",");
+  await fetchMultiplePosts(postIdsArr);
 };
 
 btnFetchPosts.addEventListener("click", handleClickSearch);
